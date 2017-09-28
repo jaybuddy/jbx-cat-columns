314d87ef0cd79af89907f378928101482b1e7796
Jay Pedersen <jay@jaybuddy.com>
2017-09-28 11:16:29 -0700

    Add my style

--- jbxCatColumns.php
+++ jbxCatColumns.php
@@ -24,89 +24,89 @@
 
 class jbxCatColumns {
 
-	echo test;
-	
-	function __construct() {
-		add_action( 'wp_enqueue_scripts', array( $this, 'jbxCatColumnsStyles' ) );
-		add_shortcode( 'jbx', array( $this, 'jbx_shortcode' ) );
-	}
+    echo test;
+    
+    function __construct() {
+        add_action( 'wp_enqueue_scripts', array( $this, 'jbxCatColumnsStyles' ) );
+        add_shortcode( 'jbx', array( $this, 'jbx_shortcode' ) );
+    }
 
-	function jbxCatColumnsStyles() {
-    	wp_enqueue_style( 'jbxCatColumnsStyles', plugin_dir_url( __FILE__ ) . 'jbxCatColumns-style.css', array(), '1.1', 'screen' );
-	}
+    function jbxCatColumnsStyles() {
+        wp_enqueue_style( 'jbxCatColumnsStyles', plugin_dir_url( __FILE__ ) . 'jbxCatColumns-style.css', array(), '1.1', 'screen' );
+    }
 
-	// [jbx columns='3' names='name1, name2, name3' numberposts='5' images='true'] //
-	function jbx_shortcode( $atts ) {
-		$a = shortcode_atts( array(
-			'columns' => '3',
-			'names' => '',
-			'numberposts' => '3',
-			'images' => 'true'
-		), $atts);
+    // [jbx columns='3' names='name1, name2, name3' numberposts='5' images='true'] //
+    function jbx_shortcode( $atts ) {
+        $a = shortcode_atts( array(
+            'columns' => '3',
+            'names' => '',
+            'numberposts' => '3',
+            'images' => 'true'
+        ), $atts);
 
-		$cats = explode( ',', str_replace( " " , "", $a['names'] ) );
+        $cats = explode( ',', str_replace( " " , "", $a['names'] ) );
 
-		//Check to make sure we have the same number of categories as columns//
-		$cat_count = count($cats);
+        //Check to make sure we have the same number of categories as columns//
+        $cat_count = count($cats);
 
-		//If we have more columns than categories, adjust the columns to be equal to categories.//
-		if ( $a['columns'] > $cat_count ) {
-			$a['columns'] = $cat_count;
-		}
-		
-		switch ($a['columns']) {
-			case 2: $col_class = 'jbx_one_half'; break;
-			case 3: $col_class = 'jbx_one_third'; break;
-			case 4: $col_class = 'jbx_one_forth'; break;
-			case 5: $col_class = 'jbx_one_fifth'; break;
-			case 6: $col_class = 'jbx_one_sixth'; break;
-		}
-		
-		$html = "";
-		$html .="<div id='jbx-wrap'>";
+        //If we have more columns than categories, adjust the columns to be equal to categories.//
+        if ( $a['columns'] > $cat_count ) {
+            $a['columns'] = $cat_count;
+        }
+        
+        switch ($a['columns']) {
+            case 2: $col_class = 'jbx_one_half'; break;
+            case 3: $col_class = 'jbx_one_third'; break;
+            case 4: $col_class = 'jbx_one_forth'; break;
+            case 5: $col_class = 'jbx_one_fifth'; break;
+            case 6: $col_class = 'jbx_one_sixth'; break;
+        }
+        
+        $html = "";
+        $html .="<div id='jbx-wrap'>";
 
-		for ($i=1; $i<=$a['columns']; $i++) { 
+        for ($i=1; $i<=$a['columns']; $i++) { 
 
-			$cat_ID = get_cat_ID( $cats[$i-1] );
-			$cat = get_category( $cat_ID );
-			
-			$posts = get_posts( array( 'posts_per_page' => $a['numberposts'], 'category_name' => $cat->name ) );
-			
-			$html .="<div class='".$col_class." jbx-block'>";
-				$html .="<h3><a href=".get_category_link($cat->term_id).">".$cat->name."</a></h3>";
-				
-				foreach ($posts as $p) {
-					$html .="<div class='jbx-item-wrap'>";
-					if ($feat == 'true') {
-						if ($n == 0) {
-							if (has_post_thumbnail( $p->ID ) ) {
-								$image = wp_get_attachment_image_src( get_post_thumbnail_id( $p->ID ), 'single-post-thumbnail' ); 
-								$html .="<div class='jbx-feat-img'><img src='".$image[0]."' alt='alt' /></div>";
-							}
-						}
-					} else {
-						if (has_post_thumbnail( $p->ID ) ) {
-							$image = wp_get_attachment_image_src( get_post_thumbnail_id( $p->ID ), 'single-post-thumbnail' ); 
-							$html .="<div class='jbx-feat-img'><img src='".$image[0]."' alt='alt' /></div>";
-						}
-					}
-					$html .="<div class='jbx-date'>".date('F d, Y', strtotime($p->post_date))."</div>";
-					$html .="<div class='jbx-title'><a href='".get_permalink($p->ID)."'>".$p->post_title."</a></div>";
-					$html .="</div>";
-				}
-				
-			$html .="</div>";
-			if ($i % $a['columns'] == 0)  {
-				$html .="<div class='jbx-clear'></div>";
-			}
-			
-		} 
-		$html .="<div class='jbx-clear'></div>";
-		$html .="</div>";
-					
-		return $html;
-	}
-	
+            $cat_ID = get_cat_ID( $cats[$i-1] );
+            $cat = get_category( $cat_ID );
+            
+            $posts = get_posts( array( 'posts_per_page' => $a['numberposts'], 'category_name' => $cat->name ) );
+            
+            $html .="<div class='".$col_class." jbx-block'>";
+                $html .="<h3><a href=".get_category_link($cat->term_id).">".$cat->name."</a></h3>";
+                
+                foreach ($posts as $p) {
+                    $html .="<div class='jbx-item-wrap'>";
+                    if ($feat == 'true') {
+                        if ($n == 0) {
+                            if (has_post_thumbnail( $p->ID ) ) {
+                                $image = wp_get_attachment_image_src( get_post_thumbnail_id( $p->ID ), 'single-post-thumbnail' ); 
+                                $html .="<div class='jbx-feat-img'><img src='".$image[0]."' alt='alt' /></div>";
+                            }
+                        }
+                    } else {
+                        if (has_post_thumbnail( $p->ID ) ) {
+                            $image = wp_get_attachment_image_src( get_post_thumbnail_id( $p->ID ), 'single-post-thumbnail' ); 
+                            $html .="<div class='jbx-feat-img'><img src='".$image[0]."' alt='alt' /></div>";
+                        }
+                    }
+                    $html .="<div class='jbx-date'>".date('F d, Y', strtotime($p->post_date))."</div>";
+                    $html .="<div class='jbx-title'><a href='".get_permalink($p->ID)."'>".$p->post_title."</a></div>";
+                    $html .="</div>";
+                }
+                
+            $html .="</div>";
+            if ($i % $a['columns'] == 0)  {
+                $html .="<div class='jbx-clear'></div>";
+            }
+            
+        } 
+        $html .="<div class='jbx-clear'></div>";
+        $html .="</div>";
+                    
+        return $html;
+    }
+    
 }
 
 $jbxCatColumns = new jbxCatColumns();
